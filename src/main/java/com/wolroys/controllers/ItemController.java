c9d3adefc9c7841f2cde3ee318fcdbf0fd9d753b
Wolroys <alekseypaslenov@mail.ru>
2023-05-29 18:13:55 +0300

    add ItemController

--- src/main/java/com/wolroys/controllers/ItemController.java
+++ src/main/java/com/wolroys/controllers/ItemController.java
@@ -1,11 +1,17 @@
 package com.wolroys.controllers;
 
+import com.wolroys.model.entities.Item;
 import com.wolroys.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+//@RequiredArgsConstructor
 @RequestMapping("/items")
 public class ItemController {
 
@@ -14,5 +20,15 @@ public class ItemController {
     @Autowired
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<Item> index(){
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("/{id}")
+    public Item showItem(@PathVariable("id") int id){
+        return itemService.getItem(id).orElse(null);
     }
 }
